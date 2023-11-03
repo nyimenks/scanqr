ce1f27ee95e35e12aa05ba0fe2ec94abcf8bb052
nyimenks <habibizakly47@gmail.com>
2023-11-03 09:36:27 +0700

    This project

--- lib/page/home_page.dart
+++ lib/page/home_page.dart
@@ -38,76 +38,40 @@ class _HomePageState extends State<HomePage> {
           ],
         ),
         drawer: Container(
-          padding: EdgeInsets.symmetric(horizontal: 7),
+          padding: EdgeInsets.symmetric(horizontal: 10, vertical: 20),
           color: Colors.white,
           width: MediaQuery.of(context).size.width - 130,
           child: ListView(
           children: [
-            Text("Gmail",
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 20,
-            ),),
+            Text("Menu", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20,),),
             Divider(),
-            buildRow(
-                icons: Icon(Icons.collections_outlined),
-                text: ("Semua kotak masuk")
-            ),
+            buildRow(icons: Icon(Icons.collections_outlined), text: ("Semua kotak masuk")),
             Divider(),
-            buildRow(
-              icons: Icon(Icons.inbox),
-              text: ("Utama")
-            ),
-            buildRow(
-                icons: Icon(Icons.discount_outlined),
-                text: ("Promosi")
-            ),
-            buildRow(
-                icons: Icon(Icons.group),
-                text: ("Sosial")
-            ),buildRow(
-                icons: Icon(Icons.info_outline),
-                text: ("Update")
-            ),
+            buildRow(icons: Icon(Icons.inbox), text: ("Utama")),
+            buildRow(icons: Icon(Icons.discount_outlined), text: ("Promosi")),
+            buildRow(icons: Icon(Icons.group), text: ("Sosial")),
+            buildRow(icons: Icon(Icons.info_outline), text: ("Update")),
             Divider(),
-            Text("Semua label"),
+            Text("Semua label", style: TextStyle(fontWeight: FontWeight.bold)),
             Divider(),
-            buildRow(
-                icons: Icon(Icons.star_border),
-                text: ("Berbintang")
-            ),
-            buildRow(
-                icons: Icon(Icons.access_time),
-                text: ("Ditunda")
-            ),
-            buildRow(
-                icons: Icon(Icons.label_important_outline),
-                text: ("Penting")
-            ),
-            buildRow(
-                icons: Icon(Icons.send_rounded),
-                text: ("Terkirim")
-            ),
-            buildRow(
-                icons: Icon(Icons.schedule_send),
-                text: ("Terjadwal")
-            ),
-            buildRow(
-                icons: Icon(Icons.send_time_extension_outlined),
-                text: ("Kotak keluar")
-            ),
-            buildRow(
-                icons: Icon(Icons.insert_drive_file_outlined),
-                text: ("Draf")
-            ),
-            buildRow(
-                icons: Icon(Icons.mail_outlined),
-                text: ("Semua email")
-            ),
-            buildRow(
-                icons: Icon(Icons.report_gmailerrorred),
-                text: ("Spam")
-            )
+            buildRow(icons: Icon(Icons.star_border), text: ("Berbintang")),
+            buildRow(icons: Icon(Icons.access_time), text: ("Ditunda")),
+            buildRow(icons: Icon(Icons.label_important_outline), text: ("Penting")),
+            buildRow(icons: Icon(Icons.send_rounded), text: ("Terkirim")),
+            buildRow(icons: Icon(Icons.schedule_send), text: ("Terjadwal")),
+            buildRow(icons: Icon(Icons.send_time_extension_outlined), text: ("Kotak keluar")),
+            buildRow(icons: Icon(Icons.insert_drive_file_outlined), text: ("Draf")),
+            buildRow(icons: Icon(Icons.mail_outlined), text: ("Semua email")),
+            buildRow(icons: Icon(Icons.report_gmailerrorred), text: ("Spam")),
+            buildRow(icons: Icon(Icons.delete), text: ("Sampah")),
+            Divider(),
+            Text("Aplikasi google", style: TextStyle(fontWeight: FontWeight.bold)),
+            Divider(),
+            buildRow(icons: Icon(Icons.calendar_today), text: ("Kalender")),
+            buildRow(icons: Icon(Icons.person_outline_rounded), text: ("Kontak")),
+            Divider(),
+            buildRow(icons: Icon(Icons.settings_outlined), text: ("Setelah")),
+            buildRow(icons: Icon(Icons.help_outline), text: ("Bantuan & masukan")),
         ],
       ),
     ),
@@ -119,7 +83,7 @@ class _HomePageState extends State<HomePage> {
             children: [
               icons,
               Padding(
-                padding: const EdgeInsets.only(bottom: 8.0, top: 8.0),
+                padding: const EdgeInsets.only(bottom: 15.0, top: 15.0),
                 child: Text(text),
             )
           ],
